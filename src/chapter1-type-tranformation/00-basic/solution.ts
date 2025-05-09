9a2047d85d9cb6451f609415d192649f7f6a4374
mjwoo1 <mjwoo@dhicc.com>
2025-05-09 11:25:06 +0900

    [chapter 1]

--- src/chapter1-type-tranformation/00-basic/solution.ts
+++ src/chapter1-type-tranformation/00-basic/solution.ts
@@ -1,9 +1,12 @@
-import {Equal, Expect} from "../../helper";
+import { Equal, Expect } from "../../helper";
+
+// typeof는 "이 변수의 타입을 그대로 가져와서 타입으로 쓰자"
+// keyof는 객체 타입에서 **모든 키(key)**를 유니언 타입으로 뽑아냅니다.
 
 const obj = {
-    hello: 'world',
-    hello2: 'world2',
-    hello3: 'world3',
+  hello: "world",
+  hello2: "world2",
+  hello3: "world3",
 };
 
 type TestingFramework = keyof typeof obj;
